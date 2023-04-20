b3258e3884a68df73c0eaab62ee03bcb0b014549
Sanskar Khandelwal <sanskarkhandelwal336@gmail.com>
2023-04-20 11:24:35 +0530

    added a simpleStorage contract to be deployed in deploy.js

--- HardHat-SimpleStorage/scripts/deploy.js
+++ HardHat-SimpleStorage/scripts/deploy.js
@@ -1,12 +1,48 @@
 //imports
-const {ethers} = require('hardhat')
+const {ethers, run, network } = require('hardhat');
 //async main
 async function main(){
   const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
   console.log("Deploying Contract");
   const simpleStorage = await SimpleStorageFactory.deploy();
   await simpleStorage.deployed()
-  console.log(simpleStorage.address);
+  console.log(`deployed contract to ${simpleStorage.address}`);
+  // console.log(network.config)
+  //what happen when we deploy to hardhat network 
+  if(network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY){
+    console.log("waiting for the block txes...")
+    await simpleStorage.deployTransaction.wait(6);
+   await verify(simpleStorage.address, [] )
+  }
+
+
+  // interacting with the contract
+  const currentValue = await simpleStorage.retrieve();
+  console.log(`Current Value is: ${currentValue}`);
+
+  //updating the current value
+  const transactionResponse = await simpleStorage.store(9);
+  await transactionResponse.wait(1);
+  const updatedValue = await simpleStorage.retrieve()
+  console.log(`Updated value is ${updatedValue}`) 
+}
+
+async function verify(contractAddress, args){
+  console.log("verifying contract...");
+  try{
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  }catch(e){
+    if(e.message.toLowerCase().incluedes("already verifired")){
+      console.log("Already verified");
+    }
+    else {
+      console.log(e)
+    }
+  }
+  
 
 }
 //main
